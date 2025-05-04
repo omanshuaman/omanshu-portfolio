d3272f004264283d330024b40dc37a637d6a6e31
Omanshuaman <omanshuaman@gmail.com>
2025-05-04 22:24:13 +0530

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./components/basics/cube-r3f/Experience";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CanvasComp from "./components/basics/cube-r3f/CanvasComp";
 import CanvasCake from "./components/basics/cake/CanvasCake";
 
 function App() {
   return (
     <>
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<CanvasComp />} />
           <Route path="/cake" element={<CanvasCake />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </>
   );
 }
